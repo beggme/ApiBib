2d9332662423ecb57020e339ab608e3d19313e8e
beggme <mehdi.beggas@gmail.com>
2015-04-01 11:33:44 +0200

    Transformation classe Resolution en classe parcelable

--- app/src/main/java/activity/Login.java
+++ app/src/main/java/activity/Login.java
@@ -3,7 +3,6 @@ package activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.example.mehdibeggas.apibib_apresreunion.R;
 
@@ -18,8 +17,6 @@ public class Login extends Activity{
     Resolution resolution;
 
     private Intent intent;
-    private int width;
-    private int height;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +31,6 @@ public class Login extends Activity{
             resolution = intent.getExtras().getParcelable(RESOLUTION);
 
         }
-
-        TextView textView = (TextView)findViewById(R.id.texte);
-        textView.setText(Integer.toString(width));
 
     }
 
--- app/src/main/java/objets/Resolution.java
+++ app/src/main/java/objets/Resolution.java
@@ -11,11 +11,13 @@ public class Resolution implements Parcelable{
     private int height;
     private int width;
 
+    //Constructeur
     public Resolution(int height, int width) {
         this.height = height;
         this.width = width;
     }
 
+    //getter setter
     public int getHeight() {
         return height;
     }
@@ -32,17 +34,45 @@ public class Resolution implements Parcelable{
         this.width = width;
     }
 
+    //from implements Parcelable
+    //utile seulement si ma classe est héritée
     @Override
     public int describeContents() {
         return 0;
     }
 
+    //from implements Parcelable
+    // liste les données à parser
     @Override
     public void writeToParcel(Parcel dest, int flags) {
 
         dest.writeInt(height);
         dest.writeInt(width);
 
+
+    }
+
+    //permet de parser ma classe
+    public static final Parcelable.Creator<Resolution> CREATOR = new Parcelable.Creator<Resolution>()
+    {
+        @Override
+        public Resolution createFromParcel(Parcel source)
+        {
+            return new Resolution(source);
+        }
+
+        @Override
+        public Resolution[] newArray(int size)
+        {
+            return new Resolution[size];
+        }
+    };
+
+    //constructeur utilisé par Parcelable.creator
+    public Resolution(Parcel in) {
+
+        this.height = in.readInt();
+        this.width = in.readInt();
 
     }
 }
