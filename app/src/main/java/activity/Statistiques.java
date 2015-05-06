c12fb38468557e7027a05981a151e1eafc25f1f6
bettke <kvbettencourt@gmail.com>
2015-05-06 23:58:55 +0200

    KBT - Ajout d'un fragment contenant le graphique (utilisation de la librairie MPAndroidChart

--- app/src/main/java/activity/Statistiques.java
+++ app/src/main/java/activity/Statistiques.java
@@ -1,8 +1,10 @@
 package activity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 import com.example.mehdibeggas.apibib_apresreunion.R;
 
@@ -11,27 +13,34 @@ import com.example.mehdibeggas.apibib_apresreunion.R;
  */
 public class Statistiques extends MenuNavigation {
 
+    FragmentManager fm;
+    FragmentTransaction ft;
+
+    Fragment_Graphique fragment_graphique;
+    Button btFragmentGraphiqueJour,btFragmentGraphiqueSemaine,btFragmentGraphiqueMois;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistiques);
 
+        btFragmentGraphiqueJour=(Button) findViewById(R.id.bt_fragmentGraphiqueJour);
+        btFragmentGraphiqueSemaine=(Button) findViewById(R.id.bt_fragmentGraphiqueMois);
+        btFragmentGraphiqueMois=(Button) findViewById(R.id.bt_fragmentGraphiqueSemaine);
+
+        btFragmentGraphiqueJour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment_graphique = new Fragment_Graphique();
+
+                fm = getFragmentManager();
+                ft = fm.beginTransaction();
+
+                ft.replace(R.id.container_fragment, fragment_graphique);
+
+                ft.commit();
+            }
+        });
     }
-
-    public void synchronisation(View view){
-        TextView textView = (TextView) findViewById(R.id.duree);
-        textView.setText("Durée : 20 minutes");
-
-        textView = (TextView) findViewById(R.id.quInitiale);
-        textView.setText("Quantité initiale : 200 ml");
-
-        textView = (TextView) findViewById(R.id.quBue);
-        textView.setText("Quantité bue : 150 ml");
-
-        textView = (TextView) findViewById(R.id.vitesse);
-        textView.setText("Vitesse : 3");
-
-    }
-
 }
