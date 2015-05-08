8fad6a1f1569a5874aff4a8adee17e0d1994c1f6
bettke <kvbettencourt@gmail.com>
2015-05-08 02:37:44 +0200

    KBT - Ajout de quelques logos

--- app/src/main/java/activity/Statistiques.java
+++ app/src/main/java/activity/Statistiques.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 
 import com.example.mehdibeggas.apibib_apresreunion.R;
 
+import java.util.HashMap;
+
 /**
  * Created by mehdibeggas on 25/03/2015.
  */
@@ -32,7 +34,65 @@ public class Statistiques extends MenuNavigation {
         btFragmentGraphiqueJour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                fragment_graphique = new Fragment_Graphique();
+
+                //Ici la requête pour récupérer les quantités bues de la journée
+                HashMap<Float,Integer>quantite = new HashMap<Float, Integer>();
+                quantite.put(4f, 0);
+                quantite.put(8f, 1);
+                quantite.put(6f, 2);
+                quantite.put(12f, 3);
+                quantite.put(18f, 4);
+                quantite.put(9f, 5);
+
+                fragment_graphique = Fragment_Graphique.newInstance(quantite);
+
+                fm = getFragmentManager();
+                ft = fm.beginTransaction();
+
+                ft.replace(R.id.container_fragment, fragment_graphique);
+
+                ft.commit();
+            }
+        });
+
+        btFragmentGraphiqueSemaine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Ici la requête pour récupérer les quantités bues de la semaine
+                HashMap<Float,Integer>quantite = new HashMap<Float, Integer>();
+                quantite.put(4f, 0);
+                quantite.put(8f, 1);
+                quantite.put(6f, 2);
+                quantite.put(12f, 3);
+                quantite.put(18f, 4);
+                quantite.put(9f, 5);
+
+                fragment_graphique = Fragment_Graphique.newInstance(quantite);
+
+                fm = getFragmentManager();
+                ft = fm.beginTransaction();
+
+                ft.replace(R.id.container_fragment, fragment_graphique);
+
+                ft.commit();
+            }
+        });
+
+        btFragmentGraphiqueMois.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Ici la requête pour récupérer les quantités bues du mois
+                HashMap<Float,Integer>quantite = new HashMap<Float, Integer>();
+                quantite.put(4f, 0);
+                quantite.put(8f, 1);
+                quantite.put(6f, 2);
+                quantite.put(12f, 3);
+                quantite.put(18f, 4);
+                quantite.put(9f, 5);
+
+                fragment_graphique = Fragment_Graphique.newInstance(quantite);
 
                 fm = getFragmentManager();
                 ft = fm.beginTransaction();
