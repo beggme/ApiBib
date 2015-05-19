92201c1fd930884ab6b0e5fbf761d3c1ef9f69ea
iyoussouf <32024121@u-paris10.fr>
2015-05-20 01:50:48 +0200

    IYO : Facebook login done. Gestion du Facebook callback en cours

--- app/src/main/java/activity/Login.java
+++ app/src/main/java/activity/Login.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.mehdibeggas.apibib_apresreunion.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 import objets.Resolution;
 
@@ -17,11 +23,13 @@ public class Login extends Activity{
     final String RESOLUTION = "resolution";
     private Intent intent;
     private Resolution resolution;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
 
         intent = getIntent();
@@ -30,6 +38,24 @@ public class Login extends Activity{
             resolution = intent.getExtras().getParcelable(RESOLUTION);
         }
 
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            // TODO gérer le callback et les hashset
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // TODO récupérer l'adresse mail
+            }
+
+            @Override
+            public void onCancel() {
+                // TODO Afficher message annuler
+            }
+
+            @Override
+            public void onError(FacebookException exc) {
+                // TODO Afficher message erreur
+            }
+        });
     }
 
     public boolean connexion(View v){
